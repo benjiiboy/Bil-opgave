f96a2d8b9201280898e5f0a311bc4698350ea51a
Anders Thomsen <ande605x@edu.easj.dk>
2017-02-14 11:42:29 +0100

    Ejerafgift beregning virker nu

--- Bil-opgaveTests/BilTests.cs
+++ Bil-opgaveTests/BilTests.cs
@@ -84,7 +84,208 @@ namespace Bil_opgave.Tests
             Assert.AreEqual(118725, dieselbil.RegristreringsAfgift());
         }
 
- 
+
+
+
+
+
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_10()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 10, false, 5);
+            Assert.AreEqual(2500, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_14()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 14, false, 5);
+            Assert.AreEqual(2500, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_15()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 15, false, 5);
+            Assert.AreEqual(1500, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_16()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 16, false, 5);
+            Assert.AreEqual(1500, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_20()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 20, false, 5);
+            Assert.AreEqual(1500, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_24()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 24, false, 5);
+            Assert.AreEqual(1500, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_25()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 25, false, 5);
+            Assert.AreEqual(1500, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_26()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 26, false, 5);
+            Assert.AreEqual(850, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Uden_30()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 30, false, 5);
+            Assert.AreEqual(850, dieselbil.HalvÅrligEjerafgift());
+        }
+
+
+
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_10()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 10, true, 5);
+            Assert.AreEqual(2000, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_14()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 14, true, 5);
+            Assert.AreEqual(2000, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_15()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 15, true, 5);
+            Assert.AreEqual(1000, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_16()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 16, true, 5);
+            Assert.AreEqual(1000, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_20()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 20, true, 5);
+            Assert.AreEqual(1000, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_24()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 24, true, 5);
+            Assert.AreEqual(1000, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_25()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 25, true, 5);
+            Assert.AreEqual(1000, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_26()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 26, true, 5);
+            Assert.AreEqual(350, dieselbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Diesel_Med_30()
+        {
+            DieselBil dieselbil = new DieselBil("Ford", "XXXXXX", 100000, 2014, 30, true, 5);
+            Assert.AreEqual(350, dieselbil.HalvÅrligEjerafgift());
+        }
+
+
+
+
+
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_10()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 10, 5);
+            Assert.AreEqual(1200, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_19()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 19, 5);
+            Assert.AreEqual(1200, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_20()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 20, 5);
+            Assert.AreEqual(600, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_21()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 21, 5);
+            Assert.AreEqual(600, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_24()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 24, 5);
+            Assert.AreEqual(600, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_27()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 27, 5);
+            Assert.AreEqual(600, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_28()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 28, 5);
+            Assert.AreEqual(600, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_29()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 29, 5);
+            Assert.AreEqual(320, benzinbil.HalvÅrligEjerafgift());
+        }
+
+        [TestMethod()]
+        public void HalvÅrligEjerafgiftTest_Benzin_35()
+        {
+            BenzinBil benzinbil = new BenzinBil("Ford", "XXXXXX", 100000, 2014, 35, 5);
+            Assert.AreEqual(320, benzinbil.HalvÅrligEjerafgift());
+        }
 
     }
 }
